77e83d82e124bff4fbffb279a672aa033599cb9d
OBenitezG <g00294642@stu.raritanval.edu>
2023-10-13 11:40:10 -0400

    Start of exercise 7.19

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -20,7 +20,8 @@ public class LogAnalyzer
         // access counts.
         hourCounts = new int[24];
         // Create the reader to obtain the data.
-        reader = new LogfileReader("demo.log");
+        //Exercise 7.12: Modify the LogAnalyzer Class:
+        reader = new LogfileReader("demoLog.txt");
     }
 
     /**
@@ -47,7 +48,7 @@ public class LogAnalyzer
             System.out.println(hour + ": " + hourCounts[hour]);
         }
     }
-    
+
     /**
      * Print the lines of data read by the LogfileReader
      */
@@ -55,4 +56,105 @@ public class LogAnalyzer
     {
         reader.printData();
     }
+
+    /**
+     * Exercise 7.14:
+     * Returns the number of accesses recorded in the log file
+     *
+     * @return    The total of all accessesss
+     */
+    public int numberOfAccessess()
+    {
+        //inital total starts at zero
+        int total = 0;
+
+        //For loops the array and adds every index into the total.
+        for(int hour = 0; hour < hourCounts.length; hour++) {
+            total += hourCounts[hour];
+        }
+
+        //Returns that total to the user.
+        return total;
+    }
+
+    /**
+     * Exercise 7.15:
+     * Comapers every hour to find which has the highest count
+     *
+     * @return    The highest count hour
+     */
+    public int busiestHour()
+    {
+        //Two variables to compare highest value to
+        int highestCount = 0;
+        int highestHour = 0;
+
+        //For loop to check every index
+        for (int index = 0; index < hourCounts.length; index++) {
+            //Compares the value of the hour to the newest highest count
+            if (hourCounts[index] > highestCount) {
+                //If the new highest count beats the old one, replace all info with its.
+                highestCount = hourCounts[index];
+                highestHour = index;
+            }
+        }
+
+        //After the array has been checked, return the highest hour
+        return highestHour;
+    }
+
+    /**
+     * Exercise 7.16:
+     * Comapers every hour to find which has the lowest count
+     *
+     * @return    The lowest count hour
+     */
+    public int quitestHour()
+    {
+        //Two variables to compare lowest value to
+        int lowestCount = hourCounts[0];
+        int lowestHour = 0;
+
+        //For loop to check every index
+        for (int index = 0; index < hourCounts.length; index++) {
+            //Compares the value of the hour to the newest lowest count
+            if (hourCounts[index] < lowestCount) {
+                //If the new lowest count beats the old one, replace all info with its.
+                lowestCount = hourCounts[index];
+                lowestHour = index;
+            }
+        }
+
+        //After the array has been checked, return the lowest hour
+        return lowestHour;
+    }
+
+    /**
+     * Exercise 7.18:     
+     * Comapers every two hours to find which has the highest copmbined count
+     * 
+     * @return    The highest count of two block period
+     */
+    public int busiestTwoHour()
+    {
+        //Two variables to compare highest value to
+        int highestCount = hourCounts[0] + hourCounts[1];
+        int highestHour = 0;
+        
+        //For loop to check every index, then add 2 to skip 1
+        for (int index = 0; index < hourCounts.length; index += 2) {
+            //current index value is added with its next index to compare its score
+            int checkCount = hourCounts[index] + hourCounts[index + 1];
+                //Compares the value of the hour to the newest highest count
+                if (checkCount > highestCount) {
+                    //If the new highest count beats the old one, replace all info with its.
+                    highestCount = checkCount;
+                    highestHour = index;
+                }
+        }
+        
+        //After the array has been checked, return the highest hour
+        return highestHour;
+    }
+
 }
